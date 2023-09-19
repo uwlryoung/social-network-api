3c2b46edd0e8707dbab21cd8d798cf17aa43082d
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2023-09-19 14:30:56 -0400

    Finished up the models, added Reaction model, and added to index.js files

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,3 +1,34 @@
-// Adding this as a model for now, but readme says: 
+const { Schema, Types} = require('mongoose');
 
-// This will not be a model, but rather will be used as the reaction field's subdocument schema in the Thought model.
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      // default: Date.now(),
+      get: (date) => {
+        if (date) return date.toISOString().split("T") [0];
+      },
+    }, 
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
